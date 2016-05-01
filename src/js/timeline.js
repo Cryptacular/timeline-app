348367ea4d2885748d0ed9b2a260a828aee11d9d
Nick Mertens <nick@thecrypt.co.nz>
2016-05-01 16:51:58 +1200

    Update template file structure. Add header section.

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,9 +1,20 @@
+// Defaults
+var defaultCategory = { name: "Default", color: 0 };
+
 // Models
-function TimelineEvent(category, name, date) {
+function TimelineEvent(data) {
     var self = this;
-    self.category = ko.observable(category);
-    self.name = ko.observable(name);
-    self.date = ko.observable(date);
+    self.category = ko.observable(data.category || defaultCategory);
+    self.name = ko.observable(data.name || "");
+    self.date = ko.observable(data.date || new Date());
+    self.description = ko.observable(data.description);
+
+    this.dateString = ko.computed(function() {
+      var day = self.date().getDate() + 1;
+      var month = self.date().getMonth() + 1;
+      var year = self.date().getFullYear();
+      return day + '/' + month + '/' + year;
+    });
 }
 
 // ViewModel
@@ -12,15 +23,17 @@ function TimelineViewModel() {
 
     // Non-editable catalog data - would come from the server
     self.categories = [
-        { name: "Default", color: 0 }
+        defaultCategory,
+        { name: "Work", color: 1 }
     ];
 
     // Editable data
     self.events = ko.observableArray([
-        new TimelineEvent(self.categories[0], "Started work", "26/04/2016"),
-        new TimelineEvent(self.categories[0], "Committed changes", "26/04/2016")
+        new TimelineEvent(self.categories[0], "Started work", new Date(2016, 4, 25)),
+        new TimelineEvent(self.categories[1], "Committed changes", new Date(2016, 4, 26))
     ]);
 
+    // Methods
     self.loginUser = function() {
       self.user.isLoggedIn(true);
     };
