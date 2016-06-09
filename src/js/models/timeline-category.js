8b642ed2b69db189a6ae1cdab178d31344a73182
Nick Mertens <nick@thecrypt.co.nz>
2016-06-09 21:30:06 +1200

    Exception thrown when data.id not provided.

--- src/js/models/timeline-category.js
+++ src/js/models/timeline-category.js
@@ -1,7 +1,21 @@
 function TimelineCategory(data) {
   var self = this;
-  self.id = data.id;
-  self.name = ko.observable(data.name || "New Category");
-  self.color = ko.observable(data.color || 0);
-  self.dates = ko.observableArray(data.dates || null);
+
+  if (data === undefined || data.id === undefined || data.id === undefined) {
+    throw "Incomplete Data Exception";
+  } else {
+    self.id = data.id;
+    self.name = ko.observable(data.name || "New Category");
+    self.color = ko.observable(data.color || "#333");
+    self.dates = ko.observableArray(data.dates || []);
+
+    self.latestDate = ko.computed(function() {
+      var latestDate;
+      var numberOfDates = self.dates().length;
+      if (numberOfDates > 0) {
+        latestDate = self.dates()[numberOfDates - 1];
+      }
+      return latestDate;
+    });
+  }
 }
